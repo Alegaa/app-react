414d91f10a65f77545a5c590f98a503c79fba2e7
gabriel.alegre <alegregabriel.90@gmail.com>
2021-12-06 22:39:23 -0300

    Creamos NavBar en React

---
+++ src/componentes/header/Header.js
@@ -0,0 +1,17 @@
+
+const Header = () =>{
+    return <>
+        <header>
+            <div>
+                <img src="../header/img/Nunzia.png" alt="" ></img>
+            </div>
+            <nav className="containerNav">
+                <a href="##">Productos</a>
+                <a href="##">Nosotros</a>
+                <a href="##">Contacto</a>
+            </nav>
+        </header>
+    </>
+}
+
+export default Header;
